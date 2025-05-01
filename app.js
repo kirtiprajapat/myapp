4e819d1278a683a1d507e38822915ecc819dc455
Kirti Prajapati <prajapatikirti342@gmail.com>
2025-05-01 13:57:21 +0530

    initial

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+ 
+  res.end('Hello, World!\n');
+});
+
+const port = 3000;
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
