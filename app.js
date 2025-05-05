a2591e04ec404923fd613109f3b76eb3c15b5758
Kirti Prajapati <prajapatikirti342@gmail.com>
2025-05-05 15:37:51 +0530

    kity

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
  
-  res.end('Hello, World!fbhcbkjdfk\n');
+  res.end('Hello, World! Prajapati \n');
 });
 
 const port = 3000;
