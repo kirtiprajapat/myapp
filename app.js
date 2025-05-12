926d192ace1dcfb6f2c9d6f932ef73bbe926ad56
Kirti Prajapati <prajapatikirti342@gmail.com>
2025-05-12 15:21:24 +0530

    ghj

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const server = http.createServer((req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
  
-  res.end('Hello, World! Prajapati \n');
+  res.end('Hello, World! Prajapatiugfgui \n');
 });
 
 const port = 3000;
 
 // Start the server and listen on the specified port
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at ${port}/`);
 });
