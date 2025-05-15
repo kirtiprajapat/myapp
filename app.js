270d977dafd2faa0e04b51ab80454a63dd30a3fa
Kirti Prajapati <prajapatikirti342@gmail.com>
2025-05-15 14:56:41 +0530

    fghj

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});  
   
-  res.end('Hello, World! Prajapati and here good friends hgfg\n');  
+  res.end('Hello, World! Prajapati \n');  
 });
 
 const port = 3000;
