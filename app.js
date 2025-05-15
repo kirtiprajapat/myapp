a331f7bccaf8d004b5fa9cd9a052cbd1b9fe6b98
Kirti Prajapati <prajapatikirti342@gmail.com>
2025-05-15 15:32:39 +0530

    jhgf

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});  
   
-  res.end('Hello, World! Prajapati \n');  
+  res.end('Hello, World! Prajapati is good girl  \n');  
 });
 
 const port = 3000;
